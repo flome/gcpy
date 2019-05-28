d18583601dcdbe3301e8778bca899399f4a08215
flome <florian.mentzel@tu-dortmund.de>
2019-05-28 16:23:08 +0200

    Create example.py

---
+++ example.py
@@ -0,0 +1,33 @@
+# import gcpy module
+import gcpy
+
+# load the current working directory
+path2data = '.'
+measurement_db = gcpy.gcdb.readDir(path2data)
+
+# compute glow curve parameters
+measurement_db.update(gcpy.gcana.calcGCparams('time_sec', 'PhCount'))
+
+# perform temperature reconstruction with 3 peaks
+measurement_db.update(gcpy.gcana.calcTreco('time_sec', 'PhCount', peaks=3))
+
+# perform glow curve deconvolution fit
+measurement_db.update(gcpy.gcana.gcFit('time_sec', 'PhCount'))
+
+# plot all curves and save them
+import matplotlib.pyplot as plt
+list_of_measurements = db.all()
+for measurement in list_of_measurements:
+  plt.plot(measurement['time_sec'], measurement['PhCount'], label='Measurement')
+  plt.xlabel('$t$ in s')
+  plt.ylabel('$N$ in $\\mathrm{\\frac{1}{5\\,ms}}$')
+  plt.legend(loc='upper right')
+  plt.savefig('gc_plot_file_%s.pdf'%measurement['filename']) 
+  # this saves each plot with its individual filename
+  plt.clf()
+  
+# delete arrays from data base and save as table
+measurement_db.update(gcana.strip_arrays)
+import pandas as pd
+measurements_dataframe = pd.DataFrame(list_of_all_meas)
+measurements_dataframe.to_excel('Results.xls')
